abc8d92150fb7d3e72600280e8ff139c3a456036
natewagner10 <54585457+natewagner10@users.noreply.github.com>
2020-10-26 15:56:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -783,6 +783,7 @@ def updateSliders(value):
 )
 def update_output(input1, input2):
     global path_to_images, path_to_mask, orientation_perc, MA_perc, ma_perc, area_perc, aspect_perc, locX_perc, locY_perc, pixs_perc
+    print(os.getcwd())
     for file_ in sorted(os.listdir(input2)):
         print(file_)
         break
